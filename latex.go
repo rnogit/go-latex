c0996691e621bda9bb649cae2967c2fd770e2f10
Remi <rnoel@rampar.com>
2020-08-23 23:26:40 +0200

    Add Cvitem

--- latex.go
+++ latex.go
@@ -10,3 +10,14 @@ func Printlist(list []string) {
 	}
 	fmt.Println("\\end{itemize}")
 }
+
+// Cvitem represents a cvitem in the modercv LaTeX template
+type Cvitem struct {
+	title   string
+	content string
+}
+
+// Printcvitem prints a LaTeX moderncv cvitem (\cvitem{title}{\textnormal{content}})
+func Printcvitem(cvitem Cvitem) {
+	fmt.Printf("\\cvitem{%s}{\textnormal{%s}}\n", cvitem.title, cvitem.content)
+}
