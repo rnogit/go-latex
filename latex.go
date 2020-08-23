80b2a7c023327172f5540d0a032614703069b86c
Remi <rnoel@rampar.com>
2020-08-23 23:13:40 +0200

    fix

--- latex.go
+++ latex.go
@@ -3,7 +3,7 @@ package latex
 import "fmt"
 
 // Printlist prints a LaTeX list (\begin{itemize} ... \end{itemize})
-func Printlist(list []int) {
+func Printlist(list []string) {
 	fmt.Println("\\begin{itemize}")
 	for _, item := range list {
 		fmt.Println("\\item", item)
