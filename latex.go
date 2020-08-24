0c6a63e34d62c1399e7233565486882a5ea89e3a
Remi <rnoel@rampar.com>
2020-08-24 03:37:58 +0200

    change

--- latex.go
+++ latex.go
@@ -1,67 +1,16 @@
 package latex
 
-import "fmt"
-
 // Cv represents the entire LaTeX resume
 type Cv struct {
 	Header                  Cvheader
-	ProfessionalExperiences []Cventry `json:"professional_experiences"`
+	ProfessionalSummary     ProfessionalSummary `json:"professional_summary"`
+	ProfessionalExperiences []Cventry           `json:"professional_experiences"`
+	Skills                  []Cvitem
+	Certifications          []Cvitem
+	Eduction                []Cvitem
+	Languages               []Cvitem
+	Interests               []Cvitem
 }
-
-// Print prints a LaTeX list (\begin{itemize} ... \end{itemize})
-func (cv Cv) Print() {
-	cv.Header.Print()
-	for _, xp := range cv.ProfessionalExperiences {
-		xp.Print()
-	}
-}
-
-// List defines a LaTeX list
-type List []string
-
-// Print prints a LaTeX list (\begin{itemize} ... \end{itemize})
-func (l List) Print() {
-	fmt.Println("\\begin{itemize}")
-	for _, item := range l {
-		fmt.Println("	\\item", item)
-	}
-	fmt.Println("\\end{itemize}")
-}
-
-// Define a \cvitem
-
-// Cvitem represents a cvitem in the modercv LaTeX package
-type Cvitem struct {
-	Title   string // The title of the cvitem
-	Content string // the content of the cvitem
-}
-
-// Print prints a LaTeX moderncv cvitem (\cvitem{title}{\textnormal{content}})
-func (i Cvitem) Print() {
-	fmt.Printf("\\cvitem{%s}{\\textnormal{%s}}\n", i.Title, i.Content)
-}
-
-// Define a \cventry
-
-// Cventry represents a cventry in the modercv LaTeX package
-type Cventry struct {
-	Date         string // The date
-	Position     string // the title or name of the position
-	Company      string // The company name
-	City         string // The city name
-	State        string // State
-	Description  string // Description of the position
-	Achievements List   // A list of achievements
-}
-
-// Print prints a LaTeX moderncv cvitem (\cvitem{title}{\textnormal{content}})
-func (e Cventry) Print() {
-	fmt.Printf("\\cventry{%s}{%s}{\\textsc{%s}}{%s, %s}{}{\\textnormal{%s}}{\n", e.Date, e.Position, e.Company, e.City, e.State, e.Description)
-	e.Achievements.Print()
-	fmt.Println("}")
-}
-
-// Define the header of the cv
 
 // Cvheader represents all the attributs of the modercv package used in the header of the resume
 type Cvheader struct {
@@ -78,13 +27,27 @@ type Cvheader struct {
 	Quote     string
 }
 
-// Print prints a LaTeX moderncv cvitem (\cvitem{title}{\textnormal{content}})
-func (h Cvheader) Print() {
-	fmt.Printf("\\firstname{%s}\n", h.Firstname)
-	fmt.Printf("\\familyname{%s}\n", h.Lastname)
-	fmt.Printf("\\address{%s}{%s %s, %s}\n", h.Address, h.Zipcode, h.City, h.Country)
-	fmt.Printf("\\mobile{%s}\n", h.Phone)
-	fmt.Printf("\\email{%s}\n", h.Email)
-	fmt.Printf("\\homepage{%s}{}\n", h.Homepage)
-	fmt.Printf("\\quote{%s}\n", h.Quote)
+// ProfessionalSummary represents the summary at the top of the CV
+type ProfessionalSummary string
+
+// Define a \cvitem
+
+// Cvitem represents a cvitem in the modercv LaTeX package
+type Cvitem struct {
+	Title   string // The title of the cvitem
+	Content string // the content of the cvitem
 }
+
+// Cventry represents a cventry in the modercv LaTeX package
+type Cventry struct {
+	Date         string // The date
+	Position     string // the title or name of the position
+	Company      string // The company name
+	City         string // The city name
+	State        string // State
+	Description  string // Description of the position
+	Achievements List   // A list of achievements
+}
+
+// List defines a LaTeX list
+type List []string
