bdf671695d0687124235cae391d81fcf586ac9bb
Remi <rnoel@rampar.com>
2020-08-25 18:37:25 +0200

    remove everytnig

--- latex.go
+++ latex.go
@@ -1,72 +1,74 @@
 package latex
 
-// Cv represents the entire LaTeX resume
+// Cv represent the entire cv
 type Cv struct {
-	Header                         Cvheader
-	ProfessionalSummary            Section                        `json:"professional_summary_section"`
-	ProfessionalExperiencesSection ProfessionalExperiencesSection `json:"professional_experiences"`
-	SkillsSection                  Section
-	CertificationsSection          CertificationsSection
-	EducationSection               Section
-	LanguagesSection               Section
-	InterestsSection               Section
+	Header struct {
+		Firstname string `json:"firstname"`
+		Lastname  string `json:"lastname"`
+		Address   string `json:"address"`
+		Zipcode   string `json:"zipcode"`
+		City      string `json:"city"`
+		State     string `json:"state"`
+		Country   string `json:"country"`
+		Phone     string `json:"phone"`
+		Email     string `json:"email"`
+		Homepage  string `json:"homepage"`
+		Quote     string `json:"quote"`
+	} `json:"header"`
+	ProfessionalSummarySection struct {
+		Title   string `json:"title"`
+		Content string `json:"content"`
+	} `json:"professional_summary_section"`
+	ProfessionalExperiencesSection struct {
+		Title       string `json:"title"`
+		Experiences []struct {
+			Name         string   `json:"name"`
+			Date         string   `json:"date"`
+			Position     string   `json:"position"`
+			Company      string   `json:"company"`
+			State        string   `json:"state"`
+			City         string   `json:"city"`
+			Description  string   `json:"description"`
+			Achievements []string `json:"achievements"`
+		} `json:"experiences"`
+	} `json:"professional_experiences_section"`
+	SkillsSection struct {
+		Title            string `json:"title"`
+		VerificationLink string `json:"verification_link"`
+		Skills           []struct {
+			Title   string `json:"title"`
+			Content string `json:"content"`
+		} `json:"skills"`
+	} `json:"skills_section"`
+	CertificationSection struct {
+		Title          string `json:"title"`
+		Certifications []struct {
+			Title   string `json:"title"`
+			Content string `json:"content"`
+		} `json:"certifications"`
+	} `json:"certification_section"`
+	EductionSection struct {
+		Title     string `json:"title"`
+		Education []struct {
+			Date        string `json:"date"`
+			College     string `json:"college"`
+			State       string `json:"state"`
+			DegreeLevel string `json:"degree_level"`
+			DegreeName  string `json:"degree_name"`
+		} `json:"education"`
+	} `json:"eduction_section"`
+	LanguagesSection struct {
+		Title     string `json:"title"`
+		Languages []struct {
+			Language    string `json:"language"`
+			Proficiency string `json:"proficiency"`
+		} `json:"languages"`
+	} `json:"languages_section"`
+	InterestsSection struct {
+		Title     string `json:"title"`
+		Languages []struct {
+			Name    string `json:"name"`
+			Content string `json:"content"`
+		} `json:"languages"`
+	} `json:"interests_section"`
 }
-
-// ProfessionalExperiencesSection represent the profesional summary section which is a little specific
-type ProfessionalExperiencesSection struct {
-	Title   string
-	Entries []Cventry
-}
-
-// CertificationsSection represents a certification section (need for the verification link)
-type CertificationsSection struct {
-	Title            string
-	VerificationLink string
-	Items            []Cvitem
-}
-
-// Section represents any section containing CVitems
-type Section struct {
-	Title string
-	Items []Cvitem
-}
-
-// Cvheader represents all the attributs of the modercv package used in the header of the resume
-type Cvheader struct {
-	Firstname string
-	Lastname  string
-	Address   string
-	Zipcode   string
-	City      string
-	State     string
-	Country   string
-	Phone     string
-	Email     string
-	Homepage  string
-	Quote     string
-}
-
-// ProfessionalSummary represents the summary at the top of the CV
-type ProfessionalSummary string
-
-// Define a \cvitem
-
-// Cvitem represents a cvitem in the modercv LaTeX package
-type Cvitem struct {
-	Title   string // The title of the cvitem
-	Content string // the content of the cvitem
-}
-
-// Cventry represents a cventry in the modercv LaTeX package
-type Cventry struct {
-	Date         string // The date
-	Position     string // the title or name of the position
-	Company      string // The company name
-	City         string // The city name
-	State        string // State
-	Description  string // Description of the position
-	Achievements List   // A list of achievements
-}
-
-// List defines a LaTeX list
-type List []string
